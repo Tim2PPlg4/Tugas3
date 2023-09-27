cebb1a0ca302b415fa02b15f834e51f4eb1c8808
Tim2PPlg4 <144776644+Tim2PPlg4@users.noreply.github.com>
2023-09-27 12:52:32 +0700

    Update Koneksi.php

--- Koneksi.php
+++ Koneksi.php
@@ -1,5 +1,5 @@
 <?php 
-$koneksi = mysqli_connect("localhost","root","","multi_user");
+$koneksi = mysqli_connect("localhost","root","","Db_website");
 
 // Check connection
 if (mysqli_connect_errno()){
